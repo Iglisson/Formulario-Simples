f52f71f21ecb86915b42c51cf2beeb62d267704d
Iglisson Ruan <Iglisson@users.noreply.github.com>
2021-01-31 17:53:59 -0300

    Incrementos 0.3
    Alterações no HTML e CSS, e inicio da função em JS

--- Formulario/_JavaScript/script.js
+++ Formulario/_JavaScript/script.js
@@ -1,3 +1,17 @@
 function enviarFormulario() {
-    alert("Olá, mundo!");
+    const nome        = formulario.nome.value; //apontando para o valor do input 'nome'
+    const email       = formulario.email.value;
+    const senha       = formulario.senha.value;
+    const confirSenha = formulario.senha2.value;
+
+
+    if (senha.length < 8) {
+        alert("A deve ter ao menos 8 digitos!");
+        formulario.senha.focus();
+        return false;
+    } else if (senha != confirSenha) {
+        alert("A senhas são diferentes!");
+        formulario.confirSenha.focus();
+        return false;
+    }
 }
